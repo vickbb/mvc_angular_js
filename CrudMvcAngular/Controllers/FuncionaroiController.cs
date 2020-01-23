fb9499a9cbfabbdb68930c7fa84e6aad241ab3a6
Victor Barreiros <victor_barreiros@hotmail.com>
2020-01-23 20:45:50 -0300

    Initial Commit

---
+++ CrudMvcAngular/Controllers/FuncionaroiController.cs
@@ -0,0 +1,24 @@
+﻿using CrudMvcAngular.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudMvcAngular.Controllers
+{
+    public class FuncionaroiController : Controller
+    {
+        #region Método para Listar Funcionário - READ
+        public JsonResult GetFuncionario()
+        {
+            using( var db = new FuncionariosEntities())
+            {
+                List<Funcionario> listarFuncionario = db.Funcionarios.ToList();
+
+                return Json(listarFuncionario, JsonRequestBehavior.AllowGet);
+            }
+        }
+        #endregion
+    }
+}
